dcb34fe653193785fbc1a04b11a8fdad5662a00b
Danylo Shapovalov <mbp13dd@icloud.com>
2022-05-12 17:22:39 +0300

    Update rusoto/services/dynamodb/src/custom/debug.rs
    Co-authored-by: Vitalii Kryvenko <veetaha2@gmail.com>

--- rusoto/services/dynamodb/src/custom/debug.rs
+++ rusoto/services/dynamodb/src/custom/debug.rs
@@ -50,7 +50,7 @@ impl fmt::Debug for AttributeValue {
         if let Some(val) = m {
             let entries = val
                 .iter()
-                .sorted_by_key(|(key, _)| *key);
+                .sorted_unstable_by_key(|(key, _)| *key);
             return f.debug_map().entries(entries).finish();
         }
 
